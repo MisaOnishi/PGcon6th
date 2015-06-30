e35e64efa2f67be9cf5f092efbdac50683c6faf0
levelfive <levelfive@level5-PC>
2015-06-30 17:10:43 +0900

    test

---
+++ procon/src/main/java/Pgcon6th/procon/App.java
@@ -0,0 +1,13 @@
+package Pgcon6th.procon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
